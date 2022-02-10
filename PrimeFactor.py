d2e3df7007b2f244fdcc5834f8380692a746f6ea
matthewDoig <71045787+matthewDoig@users.noreply.github.com>
2022-02-10 19:49:54 +0000

    Add files via upload
    Project Euler 3 PrimeFactor

--- PrimeFactor.py
+++ PrimeFactor.py
@@ -1,23 +1,22 @@
+import math
 def is_prime(factor):
-    Prime = True
-    current = 0
-    for x in range(2,int(factor)):
-        current = factor / x
-        if current.is_integer() == True:
+    prime = True
+    half = (round(factor / 2) + 1)
+    for x in range(2,int(half)):
+        if factor % x == 0:
             prime = False
             return(False)
     return(True)
-
 
 n = 1
 total = 0
 factor = []
 max = 600851475143
-for n in range(1,max):
-    current = max/n
-    if current.is_integer() == True:
-        factor.append(current)
-        total = total +1
+for n in range(1,round(math.sqrt(max))+ 1):
+    if max%n == 0:
+        factor.append(max/n)
+        factor.append(n)
+        total = total + 2
 
 for x in range(total):
     if is_prime(factor[x]) == True:
